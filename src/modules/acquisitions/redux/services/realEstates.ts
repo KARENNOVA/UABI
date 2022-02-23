57227c6879bf2fe7c8bb9cc34a2decfa7d439135
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2022-02-23 07:23:47 -0500

    Fix: Inactivate Real Estate service

--- src/modules/acquisitions/redux/services/realEstates.ts
+++ src/modules/acquisitions/redux/services/realEstates.ts
@@ -78,8 +78,6 @@ export const getRealEstate = async (
     }
 };
 
-
-
 const get_docucments_whit_service = async (docs) => {
     try {
         if (!Array.isArray(docs) && typeof docs === 'string') {
@@ -95,12 +93,12 @@ const get_docucments_whit_service = async (docs) => {
                     doc.type === 3
                         ? 'Documento de Matricula'
                         : doc.type === 4
-                            ? 'Documento de Titulo'
-                            : doc.type === 6
-                                ? 'Documento Avalúo'
-                                : doc.type === 7
-                                    ? 'Documento de Prediación'
-                                    : 'Anexo',
+                        ? 'Documento de Titulo'
+                        : doc.type === 6
+                        ? 'Documento Avalúo'
+                        : doc.type === 7
+                        ? 'Documento de Prediación'
+                        : 'Anexo',
             }));
         }
         return [];
@@ -124,14 +122,13 @@ const finalData = (data, docs_ids?) => {
     delete aux_data.registry_number_document_id;
     delete aux_data.accounting_account;
     delete aux_data.tipology;
-    console.log(aux_data.type)
+    console.log(aux_data.type);
     if (!aux_data.type) {
         delete aux_data.sap_id;
         delete aux_data.active_code;
     } else {
         delete aux_data.type;
     }
-
 
     if (aux_data.construction_area === '') {
         aux_data.construction_area = null;
@@ -145,7 +142,7 @@ const finalData = (data, docs_ids?) => {
     if (aux_data.projects_id) {
         aux_data.projects_id[0] = Number(aux_data.projects_id[0]);
     }
-    if (aux_data.patrimonial_value === "") {
+    if (aux_data.patrimonial_value === '') {
         aux_data.patrimonial_value = 0;
     }
 
@@ -167,19 +164,15 @@ const finalData = (data, docs_ids?) => {
 export const createRealEstate = async (
     data: any
 ): Promise<IRealEstateAttributes | string> => {
-    console.log('servicio antes', data)
+    console.log('servicio antes', data);
     const aux_data = finalData(data);
-    console.log('servicio despues', aux_data)
+    console.log('servicio despues', aux_data);
     try {
         let URI = `/real-estates`;
         const docs: any = await compute_docs(data.supports_documents);
         let res: AxiosResponse<IRealEstateResponse>;
         if (docs) {
-
-            res = await http.post(
-                URI,
-                aux_data
-            );
+            res = await http.post(URI, aux_data);
             const docs_ids = await upload_documents(docs, res.data.results.id);
 
             await http.put(
@@ -189,9 +182,9 @@ export const createRealEstate = async (
                     params: { id: res.data.results.id },
                 }
             );
-            console.log('enviar', res)
+            console.log('enviar', res);
         }
-        return res?.data?.results || "";
+        return res?.data?.results || '';
     } catch (error) {
         console.error(error);
         return Promise.reject('Error');
@@ -199,7 +192,12 @@ export const createRealEstate = async (
 };
 
 //TODO: Crear realEstates englobe - desenglobe
-export const createRealEstates = async (data: any, fathers: any[], action, quantity) => {
+export const createRealEstates = async (
+    data: any,
+    fathers: any[],
+    action,
+    quantity
+) => {
     console.log('crear', data);
     let realEstates = [];
     const docs = await Promise.all(
@@ -212,7 +210,7 @@ export const createRealEstates = async (data: any, fathers: any[], action, quant
         if (dr.plot_area === '') {
             dr.plot_area = 0;
         }
-        if(action !== "divide") {
+        if (action !== 'divide') {
             delete dr.value;
             delete dr.key;
             delete dr.project_id;
@@ -235,21 +233,24 @@ export const createRealEstates = async (data: any, fathers: any[], action, quant
     //         ,
     //         ...f
 
-
     //     }
     // })
     const data_final = { realEstateFather: fathers, realEstates };
-    console.log(data_final)
+    console.log(data_final);
 
     try {
         let URI = `/real-estates`;
-        let res: AxiosResponse<any> = await http.post(
-            URI,
-            data_final,
-            {
-                params: { quantity, action: action === "englobar" ? "bundle"  :  action === "desenglobar" ? "unbundle" : action },
-            }
-        );
+        let res: AxiosResponse<any> = await http.post(URI, data_final, {
+            params: {
+                quantity,
+                action:
+                    action === 'englobar'
+                        ? 'bundle'
+                        : action === 'desenglobar'
+                        ? 'unbundle'
+                        : action,
+            },
+        });
 
         const docs_ids = await Promise.all(
             docs.map(async (d, i) => {
@@ -271,16 +272,11 @@ export const createRealEstates = async (data: any, fathers: any[], action, quant
                     params: { id: r.id },
                 }
             );
-        })
+        });
 
-
-        await Promise.all(
-            assing_doc
-        ).then((res) => {
+        await Promise.all(assing_doc).then((res) => {
             console.log(res);
-        })
-
-
+        });
 
         // res.data.results.map(result => {
 
@@ -302,34 +298,28 @@ export const createRealEstates = async (data: any, fathers: any[], action, quant
 
 //TODO: Editar BIs englobe
 export const updateRealEstates = async (data: any) => {
-    data = data.map(d => {
+    data = data.map((d) => {
         return {
             ...d,
             projects_id: [d.project.id],
             active_type: d.active_type.split(', '),
             materials: d.materials.join(),
-        }
-    })
+        };
+    });
     try {
         console.log('bienes inmuebles a actualizar', data);
         let URI = `/real-estates`;
         const res: any = await Promise.all(
             data.map((r: any, i) => {
-                console.log(r)
+                console.log(r);
                 const body = finalData(r);
-                return http.put(
-                    URI,
-                    body,
-                    {
-                        params: { id: r.id },
-                    }
-                );
-
+                return http.put(URI, body, {
+                    params: { id: r.id },
+                });
             })
-        )
+        );
 
         return res.data.results;
-
     } catch (error) {
         console.error(error);
         return Promise.reject('Error');
@@ -371,7 +361,7 @@ export const updateRealEstate = async (data: any, id: number) => {
                 );
             return res.data.results;
         } else {
-            return Promise.resolve(false)
+            return Promise.resolve(false);
         }
     } catch (error) {
         console.error(error);
@@ -424,10 +414,17 @@ const getAddress = async (values) => {
 
 const deleteRealEstate = async (id) => {
     try {
-        let URI = `/real-estates/delete`;
-        let res: AxiosResponse<IRealEstateResponse> = await http.delete(URI, {
-            params: { id },
-        });
+        const URI = `/real-estates/inactivate`;
+        const body = {};
+        const res: AxiosResponse<IRealEstateResponse> = await http.patch(
+            URI,
+            body,
+            {
+                params: {
+                    id,
+                },
+            }
+        );
 
         swal_success.fire({
             title: 'Bien Inmueble Inactivado',
